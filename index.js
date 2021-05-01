05a47b6f907394030f3e2cf520c83cc7af7f623c
seireitou <ausis@mail.com>
2021-05-01 15:30:57 -0500

    commit tree 8e81c8c7dc41d4fe5383bd99be9492b0c7493e85

--- index.js
+++ index.js
@@ -1,3 +1,22 @@
 class Formatter {
   //add static methods here
+  static capitalize(string) {
+    return string[0].toUpperCase() + string.slice(1)
+  }
+
+  static sanitize(string) {
+    return string.replace(/[^A-Za-z0-9-' ]+/g, '')
+  }
+
+  static titleize(string) {
+    const dnc = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from']
+    const result = string.split(" ")
+    return result.map((word, int) => {
+      if (!dnc.includes(word) || int === 0) {
+        return Formatter.capitalize(word)
+      } else {
+        return word
+      }
+    }).join(" ")
+  }
 }
